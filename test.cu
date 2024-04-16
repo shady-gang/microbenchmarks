43de2e5e43595679e21bac36d0eeca6dc10b4e86
Hugo Devillers <devillers@cg.uni-saarland.de>
2024-04-16 17:32:45 +0200

    added CUDA reference

---
+++ test.cu
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+extern "C" {
+
+__global__ void simple_iadd_global_buffers(__device__ int32_t* a, __device__ int32_t * b , __device__ int32_t* c ) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+}
