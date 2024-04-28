8492282a1bfbfc05b8824e6a8f80a76568d0fc55
Hugo Devillers <hugo@xol.io>
2024-04-28 12:07:12 +0200

    binary_tree: added code for multiple rounds

--- binary_tree.comp.cpp
+++ binary_tree.comp.cpp
@@ -13,7 +13,11 @@ using Node = TreeNode<int>;
 compute_shader local_size(256, 1, 1)
 void treesearch(global int32_t* a, global int32_t* b, global Node* root) {
     int i = gl_GlobalInvocationID.x;
-    b[i] = !!find((Node*) root, a[i]);
+    int rounds = 1;
+    for (int k = 0; k < rounds; k++) {
+        b[i] = !!find((Node*) root, a[i]);
+        i = (i + 1) % 0x10000;
+    }
 }
 
 }
